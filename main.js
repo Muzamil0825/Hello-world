cdb13ecbec84e106fa51335188185ff706f839bc
Muzamil0825 <159967994+Muzamil0825@users.noreply.github.com>
2024-04-18 01:27:24 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+// Question1
+console.log("hello world");
+// Question2
+var myName = "Mzammil";
+var message = "is a good person";
+console.log(myName + " " + message);
---
+++ main.ts
@@ -0,0 +1,8 @@
+// Question1
+// console.log("hello world")
+
+
+// Question2
+// let myName = "Mzammil";
+// let message = "is a good person";
+// console.log(myName+ " "+message);
